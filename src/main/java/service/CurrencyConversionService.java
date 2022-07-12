b7f8fcdb1f9d8909c329389c7509700c7f91195a
Harbachou Aliaksandr <aliaksandr.harbachou@mbicycle.com>
2022-07-12 10:55:16 +0300

    "my app"

---
+++ src/main/java/service/CurrencyConversionService.java
@@ -0,0 +1,12 @@
+package service;
+
+import entities.Currency;
+import service.impl.NbrbCurrencyConversionService;
+
+public interface CurrencyConversionService {
+    static CurrencyConversionService getInstance() {
+        return new NbrbCurrencyConversionService();
+    }
+
+    double getConversionRatio(Currency original, Currency target);
+}
