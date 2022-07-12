9c8b7b6233e01363f15ec3175b8a9943cc18dfda
Harbachou Aliaksandr <aliaksandr.harbachou@mbicycle.com>
2022-07-12 11:20:06 +0300

    "my app22"

--- src/main/java/heroku/PingMe.java
+++ src/main/java/heroku/PingMe.java
@@ -1,27 +1,21 @@
 package heroku;
 
 import lombok.SneakyThrows;
-import org.springframework.scheduling.annotation.Scheduled;
 
 import java.net.HttpURLConnection;
 import java.net.URL;
+
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class PingMe {
 
-    @Scheduled(fixedRateString = "PT5M")
     @SneakyThrows
     public void ping() {
+        URL url = new URL("https://exchanger-tg-bot.herokuapp.com/");
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.connect();
 
-        try {
-            URL url = new URL("https://exchanger-tg-bot.herokuapp.com/");
-            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-            connection.connect();
-            log.info("Server is working...  Ping {} status: {}", url.getHost(), connection.getResponseCode());
-            connection.disconnect();
-        } catch (Exception e) {
-            log.error("Error from pingMe {}", e.getMessage());
-        }
+        connection.disconnect();
     }
 }
