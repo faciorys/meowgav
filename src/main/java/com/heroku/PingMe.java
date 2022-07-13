6d3daaae0b5b3f158c54d5cb2f97598eb42d42a5
Harbachou Aliaksandr <aliaksandr.harbachou@mbicycle.com>
2022-07-13 12:44:35 +0300

    "my app222"

--- src/main/java/com/heroku/PingMe.java
+++ src/main/java/com/heroku/PingMe.java
@@ -4,8 +4,8 @@ import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 
-import java.net.HttpURLConnection;
-import java.net.URL;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 
 @Slf4j
 public class PingMe {
@@ -13,10 +13,9 @@ public class PingMe {
     @Scheduled(fixedRateString = "PT20M")
     @SneakyThrows
     public void ping() {
-        URL url = new URL("https://meowgavbot.herokuapp.com/");
-        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-        connection.connect();
-
-        connection.disconnect();
+        Document document = Jsoup.connect("https://www.google.com/")
+                .userAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.84 Safari/537.36")
+                .get();
+        document.select("#SIvCob");
     }
 }
